642fbf21df93a8f04e2bfa3c1d0308e1297d0c37
Kamil Staszewski <kamilstaszewski95@gmail.com>
2018-11-13 19:09:59 +0100

    done

--- src/App.js
+++ src/App.js
@@ -1,15 +1,35 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import './App.css';
-import { startDragging } from './actions/dragActions';
+import { dropItem } from './actions/dragActions';
 
 class App extends Component {
 
-//   onDragStart = (ev, id) => {
-//     console.log('dragstart:',id);
-//     ev.dataTransfer.setData("id", id);
-// }
+  startDragging = (ev, id) => {
+    console.log('dragstart:',id);
+    ev.dataTransfer.setData("id", id);
+    }
 
+    dragOver = (ev) => {
+        ev.preventDefault();
+    }
+
+
+    // onDrop = (ev, cat) => {
+    //     let id = ev.dataTransfer.getData("id");
+    //     console.log(id)
+    //     let tasks = this.state.tasks.filter((task) => {
+    //         if (task.name == id) {
+    //             task.category = cat;
+    //         }
+    //         return task;
+    //     });
+ 
+    //     this.setState({
+    //         ...this.state,
+    //         tasks
+    //     });
+    //  }
   render() {
     var tasks = {
       wip: [],
@@ -19,7 +39,7 @@ class App extends Component {
   this.props.items.forEach ((t) => {
       tasks[t.category].push(
           <div key={t.name} 
-              onDragStart = {(e) => this.props.startDragging(e, t.name)}
+              onDragStart = {(e) => this.startDragging(e, t.name)}
               draggable
               className="draggable"
               style = {{backgroundColor: t.bgcolor}}
@@ -29,19 +49,23 @@ class App extends Component {
       );
   });
     return (
-      <div className="container-drag">
-      <h2 className="header">DRAG & DROP DEMO</h2>
-      <div className="wip">
-          <span className="task-header">WIP</span>
-          {tasks.wip}
-      </div>
-      <div className="droppable" >
-           <span className="task-header">COMPLETED</span>
-           {tasks.complete}
-      </div>
+        <div className="container-drag">
+        <h2 className="header">DRAG & DROP DEMO</h2>
+        <div className="wip"
+            onDragOver={(e)=>this.dragOver(e)}
+            onDrop={(e)=>{this.props.dropItem(e, "wip")}}>
+            <span className="task-header">WIP</span>
+            {tasks.wip}
+        </div>
+        <div className="droppable" 
+            onDragOver={(e)=>this.dragOver(e)}
+            onDrop={(e)=>this.props.dropItem(e, "complete")}>
+             <span className="task-header">COMPLETED</span>
+             {tasks.complete}
+        </div>
 
 
-  </div>
+    </div>
     )
   }
 }
@@ -50,14 +74,6 @@ const mapStateToProps = state => ({
   items: state.dragDropReducer.items
 });
 
-// export const mapDispatchToProps = dispatch => {
-//   return {
-//     startDragging: (event) => {
-//       event.dataTransfer.setData("text", event.target.id)
-//       dispatch({type: 'START_DRAG', payload: event.target.id})
-//     },
-//   };
-// };
 
-export default connect(mapStateToProps, { startDragging })(App);
+export default connect(mapStateToProps, { dropItem })(App);
 
--- src/reducers/dragDropReducer.js
+++ src/reducers/dragDropReducer.js
@@ -1,17 +1,38 @@
-import { START_DRAG } from '../constants/constants';
+import {
+  START_DROP
+} from '../constants/constants';
 
 const initialState = {
-  items: [
-    {name:"Angular", category:"wip", bgcolor: "yellow"},
-    {name:"React", category:"wip", bgcolor:"pink"},
-    {name:"Vue", category:"complete", bgcolor:"skyblue"}
+  items: [{
+      name: "Angular",
+      category: "wip",
+      bgcolor: "yellow"
+    },
+    {
+      name: "React",
+      category: "wip",
+      bgcolor: "pink"
+    },
+    {
+      name: "Vue",
+      category: "complete",
+      bgcolor: "skyblue"
+    }
   ]
 };
 
-export default function(state = initialState, action) {
+export default function (state = initialState, action) {
   switch (action.type) {
-    case START_DRAG:
-      return state
+    case START_DROP:
+      let tasks = state.items.filter((item) => {
+        if (item.name == action.payload) {
+          item.category = action.payload1;
+        }
+        return item;
+      });
+      return Object.assign({}, state, {
+        items: tasks
+      });
     default:
       return state;
   }
