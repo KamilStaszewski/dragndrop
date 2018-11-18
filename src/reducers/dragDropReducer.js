cecad4a8782de7284bae485c6d29997bc0ba06fd
Kamil Staszewski <kamilstaszewski95@gmail.com>
2018-11-18 20:46:15 +0100

    app works

--- src/reducers/dragDropReducer.js
+++ src/reducers/dragDropReducer.js
@@ -1,64 +1,99 @@
 import {
   START_DROP,
   GET_INITIAL_STATE
-} from '../constants/constants';
+} from '../constants/constants'
 
 const resettedState = {
   items: [{
-      name: "Angular",
-      category: "wip",
-      bgcolor: "yellow"
-    },
-    {
-      name: "React",
-      category: "wip",
-      bgcolor: "pink"
-    },
-    {
-      name: "Vue",
-      category: "complete",
-      bgcolor: "skyblue"
-    }
+    name: 'Angular',
+    category: 'wip',
+    bgcolor: 'yellow'
+  },
+  {
+    name: 'React',
+    category: 'wip',
+    bgcolor: 'pink'
+  },
+  {
+    name: 'Vue',
+    category: 'complete',
+    bgcolor: 'skyblue'
+  }
   ],
   count: 0
-};
+}
 
 const initialState = {
   items: [{
-      name: "Angular",
-      category: "wip",
-      bgcolor: "yellow"
-    },
-    {
-      name: "React",
-      category: "wip",
-      bgcolor: "pink"
-    },
-    {
-      name: "Vue",
-      category: "complete",
-      bgcolor: "skyblue"
-    }
+    name: 'Angular',
+    category: 'wip',
+    bgcolor: 'yellow'
+  },
+  {
+    name: 'React',
+    category: 'wip',
+    bgcolor: 'pink'
+  },
+  {
+    name: 'Vue',
+    category: 'complete',
+    bgcolor: 'skyblue'
+  }
   ],
   count: 0
-};
+}
 
 export default function (state = initialState, action) {
   switch (action.type) {
     case START_DROP:
       let tasks = state.items.filter((item) => {
         if (item.name === action.payload) {
-          item.category = action.payload1;
+          item.category = action.payload1
         }
-        return item;
-      });
+        return item
+      })
       return Object.assign({}, state, {
         items: tasks,
         count: state.count + 1
-      });
+      })
     case GET_INITIAL_STATE:
-      return state = resettedState;
+      const copiedObject = clone(resettedState)
+      return copiedObject
     default:
-      return state;
+      return state
   }
+}
+
+function clone (obj) {
+  var copy
+
+  // Handle the 3 simple types, and null or undefined
+  if (obj == null || typeof obj !== 'object') return obj
+
+  // Handle Date
+  if (obj instanceof Date) {
+    copy = new Date()
+    copy.setTime(obj.getTime())
+    return copy
+  }
+
+  // Handle Array
+  if (obj instanceof Array) {
+    copy = []
+    for (var i = 0, len = obj.length; i < len; i++) {
+      copy[i] = clone(obj[i])
+    }
+    return copy
+  }
+
+  // Handle Object
+  if (obj instanceof Object) {
+    copy = {}
+    for (var attr in obj) {
+      if (obj.hasOwnProperty(attr)) copy[attr] = clone(obj[attr])
+    }
+    return copy
+  }
+
+  throw new Error("Unable to copy obj! Its type isn't supported.")
 }
